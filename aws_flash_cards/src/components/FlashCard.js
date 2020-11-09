913522a5ada32058ff1e098e2a9fa694b378dbc5
Ramin Radmand <ramin.radmand@gmail.com>
2020-11-08 17:16:48 -0700

    added quizbar to refactor code, and clean up

---
+++ aws_flash_cards/src/components/FlashCard.js
@@ -0,0 +1,11 @@
+import React, { Componenet} from 'react'
+
+
+class FlashCard extends Componenet {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+}
--- aws_flash_cards/src/components/QuizBar.js
+++ aws_flash_cards/src/components/QuizBar.js
@@ -1,24 +1,40 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDumbbell, faFont, faFileAlt, faDice} from '@fortawesome/free-solid-svg-icons'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import QuizType from './QuizType'
  
-library.add(faDumbbell)
-library.add(faFont)
-library.add(faFileAlt)
-library.add(faDice)
+
 
 function QuizBar (props) {
 
+        const quizArray = [{
+            icon:'dice',
+            type: 'Random'
+        },
+        {
+            icon:'file-alt',
+            type: 'Regular'
+        },
+        {
+            icon: 'dumbbell',
+            type: 'Weighted'
+        },
+        {
+            icon:'font', 
+            type:'mutli'
+
+        }   
+    
+    ]
+
+    const quizTypes = quizArray.map((qt, i)=>{
+        return (
+            <QuizType key={i} icon={qt.icon} quizType={qt.type} userChoice={props.userChoice}/>
+        )
+    })
     return(
         <div className= 'quiz-bar'>
             <h1>Choose your study type</h1>
             <ul className='nav nav-pills nav-fill'>
-                <li className='col-sm-3 text-center'>
-                    <div className='nav-card'>
-                        <FontAwesomeIcon icon='dice' size='4x'/>
-                    </div>
-                </li>
+                {quizTypes}
             </ul>
         </div>
     )
